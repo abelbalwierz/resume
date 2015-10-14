c5a8f6f8d7fe43f8cb2a424754e0c8a9b148a85b
Abel Balwierz <balwierz@yahoo.com>
2015-10-14 17:40:41 -0500

    updates

---
+++ CarDealershipWebApp/CarDealership/Controllers/CarController.cs
@@ -0,0 +1,80 @@
+﻿using CarDealership.Models;
+using CarDealership.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarDealership.Controllers
+{
+    public class CarController : Controller
+    {
+        ICarRepository _repo = new MockCarRepository();
+
+        // GET: Car
+        public ActionResult Index()
+        {
+            var cars = _repo.GetAllCars();
+            return View(cars);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var car = _repo.GetCarById(id);
+            return View(car);
+        }
+
+        public ActionResult Add()
+        {
+            ViewBag.Message = "Enter a new car for sale using the form below!";
+            return View(new Car());
+        }
+
+        [HttpPost]
+        public ActionResult Add(Car newCar)
+        {
+            ViewBag.Message = "Enter a new car for sale using the form below!";
+
+            if (ModelState.IsValid)
+            {
+                _repo.AddCar(newCar);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Add");
+            }
+        }
+       
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Details2(string Year, string Make, string Model)
+        {
+            Car carView = _repo.GetCarByMMY(Year, Make, Model);
+            if (carView == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Details", carView);
+        }
+
+    [HttpPost]
+        public ActionResult LoginUser(string username, string password)
+        {
+            var user = _repo.LoginUser(username, password);
+            ViewBag.User = user;
+            if (user == null)
+            {
+                return View("Login");
+            }
+            else { 
+                var cars = _repo.GetAllCars();
+                return View("Index", cars);
+            }
+        }
+    }
+}
---
+++ CarDealershipWebApp/CarDealership/Models/ContactForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarDealership.Models
+{
+    public class ContactForm : IValidatableObject
+    {
+        // Your task is to:
+        //
+        // Make all fields required
+        // Ensure the Email field contains an '@' symbol
+        // Ensure PhoneNumber is in the format: 1-XXX-XXX-XXXX
+        // If the Income is less than 10000 and the PurchaseTimeFrameInMonts is greater than 12,
+        // generate a model level area that says 'We don't want your business!'
+
+        public string Name { get; set; }
+        public int PurchaseTimeFrameInMonths { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal? Income { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (Income == null || Income < 20000)
+            {
+                errors.Add(new ValidationResult("Income must be $20,000 or greater", new string[] { "Income" }));
+            }
+            if (Name == null)
+            {
+                errors.Add(new ValidationResult("A name must be entered", new string[] { "Name" }));
+            }
+            if (PurchaseTimeFrameInMonths < 12)
+            {
+                errors.Add(new ValidationResult("We don't want your business!", new string[] { "PurchaseTimeFrameInMonths" }));
+            }
+            if ((PhoneNumber == null) || (PhoneNumber.Length != 12) || (PhoneNumber[3] != '-') || (PhoneNumber[7] != '-'))
+            {
+                errors.Add(new ValidationResult("A phone number is required in the format xxx-xxx-xxxx", new string[] { "PhoneNumber" }));
+            }
+            if ((Email == null) || (!Email.Contains("@")))
+            {
+                errors.Add(new ValidationResult("Email is required and must contain an @ symbol", new string[] { "Email" }));
+            }
+            return errors;
+        }
+    }
+}
